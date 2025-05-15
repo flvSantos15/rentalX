594441380d4cdb9887fac81273e34e249c8c03cd
flvsantos <flvsantos300@gmail.com>
2025-05-14 23:04:45 -0300

    chore: add multer and csv-parse packages

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router()
+
+const upload = multer({ dest: "./tmp" })
 
 // @ts-ignore
 categoriesRoutes.post('/', (req, res) => {
@@ -13,6 +17,11 @@ categoriesRoutes.post('/', (req, res) => {
 // @ts-ignore
 categoriesRoutes.get('/', (req, res) => {
   return listCategoriesController.handle(req, res)
+})
+
+// @ts-ignore
+categoriesRoutes.post('/import', upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res)
 })
 
 export { categoriesRoutes };
