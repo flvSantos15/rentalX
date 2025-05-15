28339bdeb2deb9808cb296bbce1e236853e582ab
flvsantos <flvsantos300@gmail.com>
2025-05-14 23:17:29 -0300

    chore: add load categories to import usecase

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,17 +1,51 @@
 import { parse } from 'csv-parse';
 import fs from 'fs';
 import { Multer } from "multer";
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 export class ImportCategoryUseCase {
-  execute(file: Multer.File): void {
-    const stream = fs.createReadStream(file.path)
+  constructor(private categoriesRepository: CategoriesRepository) {}
 
-    const parseFile = parse()
-    
-    stream.pipe(parseFile)
+  loadCategories(file: Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
 
-    parseFile.on("data", async (line) => {
-      const [name, description] = line
+      const parseFile = parse()
+      
+      stream.pipe(parseFile)
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line
+
+          categories.push({ name, description })
+        })
+        .on("end", () => {
+          resolve(categories)
+        })
+        .on("error", (error) => {
+          reject(error)
+        })
+    })
+  }
+
+  async execute(file: Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+
+    categories.map(category => {
+      const { name, description } = category
+
+      const exisCategory = this.categoriesRepository.findByName(name)
+
+      if (!exisCategory) {
+        this.categoriesRepository.create({ name, description })
+      }
     })
   }
 }
