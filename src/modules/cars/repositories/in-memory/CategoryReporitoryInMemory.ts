69d3ed9c4bc1880485a5cc221ab2b422ef83dc05
flvsantos <flvsantos300@gmail.com>
2025-05-23 17:25:06 -0300

    chore: add creat category usecase test

---
+++ src/modules/cars/repositories/in-memory/CategoryReporitoryInMemory.ts
@@ -0,0 +1,29 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesReporitory";
+
+export class CategoryRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, { name, description });
+
+    this.categories.push(category);
+
+    return Promise.resolve();
+  }
+
+  findByName(name: string): Promise<Category> {
+    return Promise.resolve(
+      this.categories.find((category) => category.name === name),
+    );
+  }
+
+  list(): Promise<Category[]> {
+    return Promise.resolve(this.categories);
+  }
+}
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoryRepositoryInMemory } from "../../repositories/in-memory/CategoryReporitoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let repository: CategoryRepositoryInMemory;
+let createCategoryUseCase: CreateCategoryUseCase;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    repository = new CategoryRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(repository);
+  });
+
+  it("should create a new category", async () => {
+    const category = {
+      name: "Category",
+      description: "Category description",
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    const foundCategory = await repository.list();
+
+    expect(foundCategory).toHaveLength(1);
+  });
+
+  it("should throw error if category already exists", async () => {
+    const category = {
+      name: "Category",
+      description: "Category description",
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    await expect(
+      createCategoryUseCase.execute(category),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
